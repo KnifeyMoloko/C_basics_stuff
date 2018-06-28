9475c631bf555f54dd355f8a38ce9992d20c8452
KnifeyMoloko <maciej.a.cisowski@gmail.com>
2018-06-28 23:03:56 +0200

    Added 1_7_PowerFunction to the examples

---
+++ K&R/Examples/1_7_PowerFunction/cmake-build-debug/CMakeFiles/PowerFunction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PowerFunction.dir/main.c.o"
+  "PowerFunction.pdb"
+  "PowerFunction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PowerFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
